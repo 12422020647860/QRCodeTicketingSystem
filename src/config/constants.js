c71b378889978a6d527f5e682377182ab280a388
Ch1l4v3r7 <un1qu31d@gmail.com>
2023-06-12 01:40:35 +0300

    add firebase configuration

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
+  apiKey: 'AIzaSyD-8TD_zrgxtDgz2kBIziqUFsfrpYyCBU4',
+  authDomain: 'qrcodeticketingsystem.firebaseapp.com',
+  projectId: 'qrcodeticketingsystem',
+  storageBucket: 'qrcodeticketingsystem.appspot.com',
+  messagingSenderId: '451184792',
+  appId: '1:451184792:web:659bab65668706e1f8e8ed',
+  measurementId: 'G-5J78C29LB3',
 };
