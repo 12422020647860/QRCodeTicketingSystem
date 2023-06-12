30cd8fec4903865a56b2e1d0cf4a4a949ba7b3cc
Ch1l4v3r7 <un1qu31d@gmail.com>
2023-06-12 23:08:20 +0300

    add metro logo

--- src/components/sidebar/components/Brand.js
+++ src/components/sidebar/components/Brand.js
@@ -1,19 +1,18 @@
 import React from "react";
 
 // Chakra imports
-import { Flex, useColorModeValue } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 
 // Custom components
-import { HorizonLogo } from "components/icons/Icons";
 import { HSeparator } from "components/separator/Separator";
 
+import metroLogo from "assets/img/metro-logo.png"
+
 export function SidebarBrand() {
-  //   Chakra color mode
-  let logoColor = useColorModeValue("navy.700", "white");
 
   return (
-    <Flex align='center' direction='column'>
-      <HorizonLogo h='26px' w='175px' my='32px' color={logoColor} />
+    <Flex align='center' direction='column' gap={8}>
+      <Image src={metroLogo} alt='' w='175px' />
       <HSeparator mb='20px' />
     </Flex>
   );
