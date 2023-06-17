c2473f35c8265f31732a9b14ea6feb7675d54192
Ch1l4v3r7 <un1qu31d@gmail.com>
2023-06-17 23:08:44 +0300

    split firebase auth

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,9 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 
 import { firebaseConfig } from 'config/constants';
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export default auth;
+export default app;
