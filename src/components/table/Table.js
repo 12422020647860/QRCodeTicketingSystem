960315bffb4001f6e637141fd7e2e9ab1d34f2f1
Ch1l4v3r7 <un1qu31d@gmail.com>
2023-06-18 05:43:09 +0300

    add pagination to table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -3,10 +3,12 @@ import {
   Flex,
   Table,
   Tbody,
+  Tfoot,
   Td,
   Th,
   Thead,
   Tr,
+  Button,
   useColorModeValue,
 } from "@chakra-ui/react";
 // Custom components
@@ -41,13 +43,24 @@ export default (props) => {
     page,
     prepareRow,
     initialState,
+    pageOptions,
+    gotoPage,
   } = tableInstance;
-  initialState.pageSize = 11;
+  initialState.pageSize = 8;
 
   const textColor = useColorModeValue("secondaryGray.900", "white");
   const borderColor = useColorModeValue("gray.200", "whiteAlpha.100");
+  const bgButton = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+  const bgHover = useColorModeValue(
+    { bg: "secondaryGray.400" },
+    { bg: "whiteAlpha.50" }
+  );
+  const bgFocus = useColorModeValue(
+    { bg: "secondaryGray.300" },
+    { bg: "whiteAlpha.100" }
+  );
   return (
-    <Table {...getTableProps()} variant="simple" color="gray.500" mb="24px">
+    <Table {...getTableProps()} variant="simple" color="gray.500">
       <Thead>
         {headerGroups.map((headerGroup, index) => (
           <Tr {...headerGroup.getHeaderGroupProps()} key={index}>
@@ -80,6 +93,31 @@ export default (props) => {
           return <Row row={row} {...row.getRowProps()} key={index} />;
         })}
       </Tbody>
+      <Tfoot>
+        <Tr>
+          <Td colSpan={columns.length}>
+            <Flex align="center" justify="center" gap={2}>
+              {pageOptions.map((pageOption) => (
+                <Button
+                  align="center"
+                  justifyContent="center"
+                  bg={bgButton}
+                  _hover={bgHover}
+                  _focus={bgFocus}
+                  _active={bgFocus}
+                  w="37px"
+                  h="37px"
+                  lineHeight="100%"
+                  borderRadius="10px"
+                  onClick={(e) => gotoPage(pageOption)}
+                >
+                  {pageOption + 1}
+                </Button>
+              ))}
+            </Flex>
+          </Td>
+        </Tr>
+      </Tfoot>
     </Table>
   );
 };
